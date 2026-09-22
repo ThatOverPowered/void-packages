b231cb918d9d99df9f5310915bc65e5fed520783
ThatOverPowered <thatoverpowered@void.localdomain>
2026-09-22 14:37:15 +0100

    Commit inicial
    Se han añadido los siguientes paquetes:
    - coolercontrold | v5.0.1_1
    - lact | v0.10.1_1
    - linux-firmware-mediatek | v20260916_1
    - linux7.2-cachyos | v7.2.6_1
    - scx-scheds | v1.1.3_1
    - scx-tools | v1.1.3_1
    - vesktop | v1.6.7_1
    
    Otros:
    - GitHub CI

---
+++ srcpkgs/vesktop/files/vesktop.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
+
+flags=""
+for _FLAGS_FILE in \
+	"${XDG_CONFIG_HOME}/electron-flags.conf" \
+	"${XDG_CONFIG_HOME}/@appname@-flags.conf" \
+	"${XDG_CONFIG_HOME}/@cfgdirname@/@appname@-flags.conf"
+do
+	if [ -f "$_FLAGS_FILE" ]; then
+		echo "Loading flags from $_FLAGS_FILE"
+		_file_flags="$(grep -v '^[[:space:]]*#' "$_FLAGS_FILE" | grep -v '^[[:space:]]*$')"
+		if [ -n "$_file_flags" ]; then
+			if [ -z "$flags" ]; then
+				flags="$_file_flags"
+			else
+				flags="$flags $_file_flags"
+			fi
+		fi
+	fi
+done
+
+exec /opt/@appname@/@appname@ $flags "$@"
